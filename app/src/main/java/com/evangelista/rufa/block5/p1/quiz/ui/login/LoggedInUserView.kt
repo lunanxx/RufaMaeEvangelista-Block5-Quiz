e56c418a64655967a035a69f673a52453c135398
lunanxx <ruub.evangelista.up@phinmaed.com>
2023-12-08 16:17:41 +0800

    not yet finished

---
+++ app/src/main/java/com/evangelista/rufa/block5/p1/quiz/ui/login/LoggedInUserView.kt
@@ -0,0 +1,9 @@
+package com.evangelista.rufa.block5.p1.quiz.ui.login
+
+/**
+ * User details post authentication that is exposed to the UI
+ */
+data class LoggedInUserView(
+    val displayName: String
+    //... other data fields that may be accessible to the UI
+)
---
+++ app/src/main/java/com/evangelista/rufa/block5/p1/quiz/ui/login/LoginFormState.kt
@@ -0,0 +1,10 @@
+package com.evangelista.rufa.block5.p1.quiz.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(
+    val usernameError: Int? = null,
+    val passwordError: Int? = null,
+    val isDataValid: Boolean = false
+)
